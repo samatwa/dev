317edbdbc0190bcb578fba3eb4c94835d981eb9e
ViacheslavKuznetsov <155986232+samatwa@users.noreply.github.com>
2026-01-22 19:47:17 +0100

    changes

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-kvv"
-    key            = "lesson-5/terraform.tfstate"
+    key            = "lesson-7/terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
