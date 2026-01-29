07488b9c96a64ef3617bf32617cc495619acae7f
ViacheslavKuznetsov <155986232+samatwa@users.noreply.github.com>
2026-01-29 16:20:02 +0100

    add module RDS

--- main.tf
+++ main.tf
@@ -12,13 +12,13 @@ module "vpc" {
   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   availability_zones = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
-  vpc_name           = "lesson-8-9-vpc"
+  vpc_name           = "lesson-db-module-vpc"
 }
 
 # Підключаємо модуль ECR
 module "ecr" {
   source      = "./modules/ecr"
-  ecr_name    = "lesson-8-9-ecr"
+  ecr_name    = "lesson-db-module-ecr"
   scan_on_push = true
 }
 
@@ -48,3 +48,43 @@ module "argo_cd" {
   namespace    = "argo-cd"
   chart_version = "5.46.4"
 }
+
+module "rds" {
+  source = "./modules/rds"
+
+  name                       = "myapp-db"
+  use_aurora                 = false
+  aurora_instance_count      = 2
+
+  # --- Aurora-only ---
+  engine_cluster             = "aurora-postgresql"
+  engine_version_cluster     = "15.3"
+  parameter_group_family_aurora = "aurora-postgresql15"
+
+  # --- RDS-only ---
+  engine                     = "postgres"
+  engine_version             = "17.2"
+  parameter_group_family_rds = "postgres17"
+
+  # Common
+  instance_class             = "db.t3.medium"
+  allocated_storage          = 20
+  db_name                    = "myapp"
+  username                   = "postgres"
+  password                   = "admin123AWS23"
+  subnet_private_ids         = module.vpc.private_subnets
+  subnet_public_ids          = module.vpc.public_subnets
+  publicly_accessible        = true
+  vpc_id                     = module.vpc.vpc_id
+  multi_az                   = true
+  backup_retention_period    = 7
+  parameters = {
+    max_connections              = "200"
+    log_min_duration_statement   = "500"
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "myapp"
+  }
+} 
